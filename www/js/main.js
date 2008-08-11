313a63f819141a72dfd33bac869ceec8cedde7a9
Yann Duponchel <yduponchel>
2008-08-11 11:40:16 +0000

    adding the web site

---
+++ www/js/main.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($) {
+ 
+  $("a[rel*=tabs-menu]").each(function () {
+  	  $(this).click(function(){
+  	  	$("#tabs-content").html($("#load").html())
+  	  	$.get(this.href, function(data) { 
+  	  		$("#content3").html(data)
+  	  	})
+  	  	$(".active").removeClass("active").addClass("inactive")
+  	  	$(".extra-large-active").removeClass("extra-large-active").addClass("inactive")
+  	  	
+	  	$(this).parents(".inactive:not(.extra-large)").removeClass("inactive").addClass("active")
+	  	$(this).parents(".extra-large", ".inactive").removeClass("inactive").addClass("extra-large-active")
+
+  	  	$(".active-tab-link").removeClass("active-tab-link").addClass("inactive-tab-link") 
+  	  	$(this).removeClass("inactive-tab-link").addClass("active-tab-link")	 	
+  	  	return false
+  	  })
+  })
+  $("#tabs-content").html($("#load").html())
+  $.get("productInfo.html", function(data) { 
+	 $("#tabs-content").html(data)
+  })
+
+})
+ 
